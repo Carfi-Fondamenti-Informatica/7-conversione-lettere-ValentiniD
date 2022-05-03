45d187121c9698fc6be06df6e3bd8487e2f7f818
ValentiniD <100331774+ValentiniD@users.noreply.github.com>
2022-05-03 18:02:37 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -4,7 +4,7 @@
 
 #include "lib.h"
 
-bool lettere(int a) {
+bool lettere(char a) {
     if (((a>=65)&&(a<=90))||(a>=97)&&(a<=122)) {
         return true;
     } else {
@@ -12,7 +12,7 @@ bool lettere(int a) {
     }
 }
 
-char conversione (int a) {
+char conversione (char a) {
     char operazione;
     if ((a>=65)&&(a<=90)) {
         operazione=a+32;
