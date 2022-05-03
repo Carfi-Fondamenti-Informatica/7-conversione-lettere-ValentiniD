80a256f94f68eebd72f6e0749a66ef5213a934d6
ValentiniD <100331774+ValentiniD@users.noreply.github.com>
2022-05-03 17:49:39 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+using namespace std;
+
+int main() {
+    char a;
+    cout << "Inserire un carattere: ";
+    cin >> a;
+    if (lettere (a)) {
+        cout << conversione (a);
+    } else {
+        cout << "errore";
+    }
+    return 0;
 }
