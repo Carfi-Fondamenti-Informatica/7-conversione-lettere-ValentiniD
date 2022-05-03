79761eeb95e198c42fe59534555d2a7346f7ea50
ValentiniD <100331774+ValentiniD@users.noreply.github.com>
2022-05-03 17:48:42 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,24 @@
+//
+// Created by gasta on 03/05/2022.
+//
 
+#include "lib.h"
+
+bool lettere(char a) {
+    if (((a>=65)&&(a<=90))||(a>=97)&&(a<=122)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+char conversione (char a) {
+    char operazione;
+    if ((a>=65)&&(a<=90)) {
+        operazione=a+32;
+        return operazione;
+    } else if ((a>=97)&&(a<=122)) {
+        operazione=a-32;
+        return operazione;
+    }
+}
